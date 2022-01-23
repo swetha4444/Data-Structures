c694e36c6a87c1ee4cca82bf06721330f96fcaee
true.living <68152189+swetha4444@users.noreply.github.com>
2022-01-23 17:48:08 +0530

    Add files via upload

--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -3,23 +3,24 @@ arr = [4,3,2,1]
 def partition(arr,pivot):
     l = 0
     r = len(arr)-1
-    while(l < r):
-        while(arr[l] < pivot):
-            l+=1
-        while (arr[r] > pivot):
-            r-=1
+    while(l<r):
+        while(arr[l]<pivot):
+            l += 1
+        while(arr[r]>pivot):
+            r -= 1
         arr[l],arr[r] = arr[r],arr[l]
-        l+=1
-        r-=1
-    return l            #division index
+        l += 1
+        r -= 1
+    return l
+
 
 def quickSortRec(arr,l,r):
     if(l>=r):
         return arr
     pivot = arr[(l+r)//2]
     index = partition(arr,pivot)
-    quickSortRec(arr,l,index-1) #left arr
-    quickSortRec(arr,index,r) #right arr
+    quickSortRec(arr,l,index-1)
+    quickSortRec(arr,index,r)
     
 
 quickSortRec(arr,0,len(arr)-1)
